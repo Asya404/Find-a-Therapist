cf5f9d9237ae525067fd745a1421b25a12708693
Asya404 <unkiplum@gmail.com>
2023-10-31 15:08:06 +0100

    feat: add base card component

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import { createApp } from 'vue';
 import router from './router.js';
 import store from './store/store.js';
 import App from './App.vue';
+import BaseCard from './components/ui/BaseCard.vue';
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
+app.component('base-card', BaseCard);
 app.mount('#app');
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,18 +1,20 @@
 <template>
   <section>Filter</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :coach="coach"
-      ></coach-item>
-    </ul>
-    <h3 v-else>No coaches found</h3>
+    <base-card>
+      <div class="controls">
+        <button>Refresh</button>
+        <router-link to="/register">Register as Coach</router-link>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :coach="coach"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found</h3>
+    </base-card>
   </section>
 </template>
 
