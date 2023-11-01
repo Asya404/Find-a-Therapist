1a54729c42c9e724234473aed00a6a535c229c9c
Asya404 <unkiplum@gmail.com>
2023-11-01 14:27:40 +0100

    feat: add base badge component

--- src/components/coaches/CoachItem.vue
+++ src/components/coaches/CoachItem.vue
@@ -3,7 +3,7 @@
     <h3>{{ fullName }}</h3>
     <h4>${{ coach.hourlyRate }}/hour</h4>
     <div>
-      <span v-for="area in coach.areas" :key="area">{{ area }}</span>
+      <base-badge v-for="area in coach.areas" :key="area" :type="area" :text="area"></base-badge>
     </div>
     <div class="actions">
       <base-button mode="outline" link="true" :to="coachContactLink">Contact</base-button>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store/store.js';
 import App from './App.vue';
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
 
 const app = createApp(App);
 app.use(router);
@@ -12,5 +13,6 @@ app.use(store);
 
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
 
 app.mount('#app');
