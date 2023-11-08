41042b2914e79c250fd066aafe51a4ec5050fe82
Asya404 <unkiplum@gmail.com>
2023-11-08 13:53:52 +0100

    feat: add form validation

--- src/pages/coaches/CoachForm.vue
+++ src/pages/coaches/CoachForm.vue
@@ -3,34 +3,46 @@
     <base-card>
       <h2>Register as a coach now</h2>
       <form @submit.prevent="submitForm">
-        <div class="form-control">
+        <div class="form-control" :class="{ invalid: !firstName.isValid }">
           <label for="firstname">First Name</label>
-          <input type="text" id="firstname" v-model.trim="firstName" />
+          <input type="text" id="firstname" v-model.trim="firstName.val" />
+          <p class="message" v-if="!firstName.isValid">
+            First Name must not be empty
+          </p>
         </div>
-        <div class="form-control">
+        <div class="form-control" :class="{ invalid: !lastName.isValid }">
           <label for="lastname">Last Name</label>
-          <input type="text" id="lastname" v-model.trim="lastName" />
+          <input type="text" id="lastname" v-model.trim="lastName.val" />
+          <p class="message" v-if="!lastName.isValid">
+            Last Name must not be empty
+          </p>
         </div>
-        <div class="form-control">
+        <div class="form-control" :class="{ invalid: !description.isValid }">
           <label for="description">Description</label>
           <textarea
             id="description"
             rows="5"
-            v-model.trim="description"
+            v-model.trim="description.val"
           ></textarea>
+          <p class="message" v-if="!description.isValid">
+            Description must not be empty
+          </p>
         </div>
-        <div class="form-control">
+        <div class="form-control" :class="{ invalid: !rate.isValid }">
           <label for="rate">Hourly rate</label>
-          <input type="number" id="rate" v-model.number="rate" />
+          <input type="number" id="rate" v-model.number="rate.val" />
+          <p class="message" v-if="!rate.isValid">
+            Rate must be greater than 0
+          </p>
         </div>
-        <div class="form-control">
+        <div class="form-control" :class="{ invalid: !areas.isValid }">
           <h3>Areas of Expertise</h3>
           <div>
             <input
               type="checkbox"
               id="frontend"
               value="frontend"
-              v-model="areas"
+              v-model="areas.val"
             />
             <label for="frontend">Frontend Development</label>
           </div>
@@ -39,15 +51,24 @@
               type="checkbox"
               id="backend"
               value="backend"
-              v-model="areas"
+              v-model="areas.val"
             />
             <label for="backend">Backend Development</label>
           </div>
           <div>
-            <input type="checkbox" id="career" value="career" v-model="areas" />
+            <input
+              type="checkbox"
+              id="career"
+              value="career"
+              v-model="areas.val"
+            />
             <label for="career">Career Advisory</label>
           </div>
+          <p class="message" v-if="!areas.isValid">
+            At least one expertise must be selected
+          </p>
         </div>
+        <p class="message" v-if="!formIsValid">Please fix the above errors</p>
         <base-button>Register</base-button>
       </form>
     </base-card>
@@ -58,15 +79,60 @@
 export default {
   data() {
     return {
-      firstName: '',
-      lastName: '',
-      description: '',
-      rate: null,
-      areas: [],
+      firstName: {
+        val: '',
+        isValid: true,
+      },
+      lastName: {
+        val: '',
+        isValid: true,
+      },
+      description: {
+        val: '',
+        isValid: true,
+      },
+      rate: {
+        val: null,
+        isValid: true,
+      },
+      areas: {
+        val: [],
+        isValid: true,
+      },
+      formIsValid: true,
     };
   },
   methods: {
+    validateForm() {
+      this.formIsValid = true;
+      if (this.firstName.val === '') {
+        this.firstName.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.lastName.val === '') {
+        this.lastName.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.description.val === '') {
+        this.description.isValid = false;
+        this.formIsValid = false;
+      }
+      if (!this.rate.val || this.rate.val < 0) {
+        this.rate.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.areas.val.length === 0) {
+        this.areas.isValid = false;
+        this.formIsValid = false;
+      }
+    },
     submitForm() {
+      this.validateForm();
+
+      if (!this.formIsValid) {
+        return;
+      }
+
       const formData = {
         first: this.firstName,
         last: this.lastName,
@@ -76,7 +142,7 @@ export default {
       };
 
       this.$store.dispatch('coaches/registerCoachAction', formData);
-      this.$router.replace('/coaches')
+      this.$router.replace('/coaches');
     },
   },
 };
@@ -129,8 +195,10 @@ h3 {
   font-size: 1rem;
 }
 
-.invalid label {
+.message {
   color: red;
+  font-size: 12px;
+  margin: 5px 0 20px 0;
 }
 
 .invalid input,
