9cdb90821eaaba8bf135489b676a4e015444499d
Asya404 <unkiplum@gmail.com>
2023-11-13 11:57:27 +0100

    feat: store messages with vuex

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,13 +1,16 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div class="form-control">
       <label for="email">Your email</label>
-      <input type="email" id="email" />
+      <input type="email" id="email" v-model.trim="request.email" />
     </div>
     <div class="form-control">
       <label for="message">Message</label>
-      <textarea id="message" rows="5"></textarea>
+      <textarea id="message" rows="5" v-model.trim="request.message"></textarea>
     </div>
+    <p class="errors" v-if="!request.formIsValid">
+      Please enter a valid email and non-empty message.
+    </p>
     <div class="actions">
       <base-button>Send message</base-button>
     </div>
@@ -57,3 +60,34 @@ textarea:focus {
   text-align: center;
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      request: {
+        coachId: this.$route.params.id,
+        email: '',
+        message: '',
+        formIsValid: true,
+      },
+    };
+  },
+  methods: {
+    submitForm() {
+      this.request.formIsValid = true;
+
+      if (
+        this.request.email === '' ||
+        !this.request.email.includes('@') ||
+        this.request.message === ''
+      ) {
+        this.request.formIsValid = false;
+        return;
+      }
+      this.$store.dispatch('requests/contactCoach', this.request);
+      this.$router.replace('/coaches');
+    },
+  },
+};
+</script>
--- src/store/modules/requests.js
+++ src/store/modules/requests.js
@@ -1,1 +1,25 @@
-export default {};
+export default {
+  namespaced: true,
+  state() {
+    return {
+      requests: [],
+    };
+  },
+  mutations: {
+    addRequest(state, payload) {
+      state.requests.push(payload);
+    },
+  },
+  actions: {
+    contactCoach(context, payload) {
+      const newRequest = {
+        id: new Date().toISOString(),
+        coachId: payload.coachId,
+        userEmail: payload.email,
+        message: payload.message,
+      };
+
+      context.commit('addRequest', newRequest);
+    },
+  },
+};
